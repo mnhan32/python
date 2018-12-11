303cc8eb2c592e4e8ead02d5805bab8ca086f342
Menghan Ho <mnhan32@gmail.com>
2018-12-11 12:15:38 +0800

    Update updateNestedDict.py
     detect if ori and udpate value are the same first. fix bug on getUpdateNestedDict

--- updateNestedDict.py
+++ updateNestedDict.py
@@ -5,39 +5,52 @@ def getUpdateNestedDict(ori, update, matchType=0, addNonExisted=True):
     oriCopy=ori.copy()
     for k in update.keys():
         if k in oriCopy.keys():
-            if isinstance(oriCopy[k], dict) and isinstance(update[k], dict):              
-                updateNestedDict(oriCopy[k], update[k], matchType=matchType, addNonExisted=addNonExisted)
-            else:
-                if not matchType:
-                    oriCopy[k] = update[k]
+            if oriCopy[k] != update[k]:            
+                if isinstance(oriCopy[k], dict) and isinstance(update[k], dict):                
+                    oriCopy[k] = getUpdateNestedDict(oriCopy[k], update[k], matchType=matchType, addNonExisted=addNonExisted)
                 else:
-                    if type(oriCopy[k]) == type(update[k]):
-                        oriCopy[k] = update[k] 
-        else:
+                    if not matchType:
+                        pass#print('In non match type mode, key %s has differnt value type than target dictionary, overwrite.'%k, 3)
+                        oriCopy[k] = update[k]
+                    else:
+                        if type(oriCopy[k]) == type(update[k]):
+                            pass#print('Update key %s value, was %s now %s.'%(k, oriCopy[k], update[k]))
+                            oriCopy[k] = update[k]
+            else:
+                pass#print('key %s has the same value, skip.'%k)                
+        else:            
             if addNonExisted:
+                pass#print('In add non existed mode, key %s not exist in target dictionary, add it to target dictionary.'%k, 3)
                 oriCopy[k] = update[k]
+            else:
+                pass#pLog('key %s not exist in target dictionary, skip.'%(pKey+':'+k), 3)
     return oriCopy
 
-
-def updateNestedDict(ori, update, matchType=0, addNonExisted=True):
+def updateNestedDict(ori, update,  matchType=1, addNonExisted=False):
     '''
     update original input dictionary
     '''
     for k in update.keys():
         if k in ori.keys():
-            if isinstance(ori[k], dict) and isinstance(update[k], dict):              
-                updateNestedDict(ori[k], update[k], matchType=matchType, addNonExisted=addNonExisted)
-            else:
-                if not matchType:
-                    ori[k] = update[k]
+            if ori[k] != update[k]:            
+                if isinstance(ori[k], dict) and isinstance(update[k], dict):                
+                    updateNestedDict(ori[k], update[k], matchType=matchType, addNonExisted=addNonExisted)
                 else:
-                    if type(ori[k]) == type(update[k]):
-                        ori[k] = update[k] 
-        else:
+                    if not matchType:
+                        pass#print('In non match type mode, key %s has differnt value type than target dictionary, overwrite.'%k, 3)
+                        ori[k] = update[k]
+                    else:
+                        if type(ori[k]) == type(update[k]):
+                            pass#print('Update key %s value, was %s now %s.'%(k, ori[k], update[k]))
+                            ori[k] = update[k]
+            else:
+                pass#print('key %s has the same value, skip.'%k)                
+        else:            
             if addNonExisted:
+                pass#print('In add non existed mode, key %s not exist in target dictionary, add it to target dictionary.'%k, 3)
                 ori[k] = update[k]
-    #return ori
-
+            else:
+                pass#pLog('key %s not exist in target dictionary, skip.'%(pKey+':'+k), 3)
 
 //Test script
 import json 
